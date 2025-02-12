41b98fcb11fbeb1fec23318d53678b4ad733c73c
Aritra25 <aritra.maji2001@gmail.com>
2025-02-12 12:35:48 +0530

    first commit2

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    mongoose.connection.on('connected', () => {
-        console.log('Connected to MongoDB');
-    })
-    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`)
+    // mongoose.connection.on('connected', () => {
+    //     console.log('Connected to MongoDB');
+    // })
+    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`).then(() => console.log("connected to MongoDB")).catch(() => console.log("Failed to connect to MongoDB"));
 }
 
 export default connectDB;
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 await connectDB()
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello, World!' });
+  res.send('Hello, World!');
 });
 
 app.listen(PORT, () => {
